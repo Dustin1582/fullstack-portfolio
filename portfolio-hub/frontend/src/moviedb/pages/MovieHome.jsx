791fd0b1e79186957fbb71a41e5e6ea0513559f8
Dustin Routh <dustinsrou@gmail.com>
2026-03-09 13:38:03 -0400

    fix

--- portfolio-hub/frontend/src/moviedb/pages/MovieHome.jsx
+++ portfolio-hub/frontend/src/moviedb/pages/MovieHome.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-
-import '../css/MovieHome.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '../components/Header';
 import Body from '../components/Body';
+import '../css/MovieHome.css'
+
 
 const MovieHome = ({
     searchedMovies, 
@@ -13,6 +13,7 @@ const MovieHome = ({
     trending
 
 }) => {
+    const nav = useNavigate();
     const [movies, setMovies] = useState(null);
 
     if(movies !== null) {
@@ -24,10 +25,10 @@ const MovieHome = ({
     <div className="body-container">
         <div className='movie-main-container' style={{backgroundImage: `url(${import.meta.env.BASE_URL}moviedb/movie_background.png)`}}>
             <div className="back-button-movie"
-            onClick={() => nav('/')}
-        >
-            <p>X</p>
-        </div>
+                onClick={() => nav('/')}
+            >
+                <p>X</p>
+            </div>
             <Header setSearchedMovies={setSearchedMovies}/>
             <Body 
                 searchedMovies={searchedMovies}
